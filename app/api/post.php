aba86f1d85bc2b33d90654543450e8804f0e9300
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-01 17:20:39 +0530

    update post but not working as i aspected

--- app/api/post.php
+++ app/api/post.php
@@ -76,20 +76,33 @@ $app->post('/api/posts/add',function(Request $request, Response $response, array
     }
 });
 
-// delete post 
-$app->get('/api/posts/delete/id',function(Request $request, Response $response, array $args){
-    $id = $request->getParam('id');
+// update post 
+$app->put('/api/posts/update/{id}',function(Request $request, Response $response){
+    // grab id
+    $id = $request->getAttribute('id');
     
-    $sql = "delete from posts where id = '{$id}'";
+    $title = $request->getParam('title');
+    $cat_id = $request->getParam('cat_id');
+    $body = $request->getParam('body');
+
+
+    $sql = "UPDATE posts SET cat_id = :cat_id , title = :title, body = :body WHERE id = $id";
 
     try{
     //Get db objet 
         $db = new DB;
         $db = $db->connection();
         $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':title',$title);
+        $stmt->bindParam(':cat_id',$cat_id);
+        $stmt->bindParam(':body',$body);
         $stmt->execute();
-        return json_encode("data Successfully delete");   
+        
+        echo  json_encode("Post Successfully Update '{$id}' ");
+           print_r($stmt);
     }catch(PDOException $e){
-            echo json_encode($e->getMessage());
+             echo $e->getMessage();
+             print_r($stmt);
     }
 });
