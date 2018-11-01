a2bf8c47a520ddc827b875c57c8b7ae9e24c6931
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-01 14:13:43 +0530

    check for urls

--- public/index.php
+++ public/index.php
@@ -5,10 +5,23 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
 
-    return $response;
-});
+$_SERVER['REQUEST_URI_PATH'] = parse_url($_SERVER['REQUEST_URI'] ,PHP_URL_PATH);
+$segmets = explode("/",$_SERVER['REQUEST_URI_PATH']);
+// echo $segmets[1] = api
+// echo $segmets[2] = post
+
+if(isset($segmets[2])){
+    $page_name = $segmets[2];
+    if($page_name == 'category' || $page_name == 'categories'){
+        require_once('../app/api/category.php');
+    }elseif($page_name == 'post' || $page_name == 'posts'){
+        require_once('../app/api/post.php');
+    }
+}else{
+    die('Please Use Api');
+}
+
+
+
 $app->run();
