b7fa88072b3ea95ad53179d732ff9d89a12dccb6
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-01 14:43:59 +0530

    Create Class to connect to db

---
+++ app/db.php
@@ -0,0 +1,22 @@
+<?php 
+
+class DB 
+{
+    private $db_host = 'localhost';
+    private $db_user = 'root';
+    private $db_name = 'blog_api';
+    private $db_pass =  '';
+
+
+    // conncection
+
+    public function connection(){
+        $mysql_connection_string = "mysql:host=$this->db_host;dbname=$this->db_name";
+
+        $db_connection = new PDO($mysql_connection_string, $this->db_user,$this->db_pass);
+
+        $db_connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRORMODE_EXCEPTION);
+        return $db_connection;
+    }
+
+}
